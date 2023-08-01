42fb239759e9efce371e3d30d50a58de43e53f97
Jadarius Hill <theemrstallion@Jadariuss-iMac.attlocal.net>
2023-08-01 16:11:46 -0500

    fix: remove terraform backend provider in main.tf

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,20 +1,10 @@
 provider "aws" {
-
-  region     = "us-east-1"
 }
 
 terraform {
   backend "s3" {
-    bucket = "blockapps-server-backend-prod"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-    skip_credentials_validation = true
   }
 }
-
-# module "env_test" {
-#   count          = var.env == "test" ? 1 : 0
-# }
 
 module "env_prod" {
   count            = var.env == "prod" ? 1 : 0
