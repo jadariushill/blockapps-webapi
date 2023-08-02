03a837364f69d54b91c3ae6cb8fd694e191f7344
jadariushill <jjh65@students.uwf.edu>
2023-08-02 00:34:28 -0500

    feat: add react client tests

---
+++ src/test/cypress/e2e/client-tests/clienttests.cy.js
@@ -0,0 +1,31 @@
+describe("BlockApps WebAPI React Tests", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+    it("Displays Header", () => {
+        cy.get("h1").should("have.text", "BlockApps Release WebAPI");
+      });
+    it("Displays Text Input", () => {
+        cy.get(".MuiInputBase-root").should('be.visible')
+    });
+    it("Displays Submit Button", () => {
+        cy.get(".MuiButtonBase-root").should('be.visible')
+    });
+    it("Should Display Time When Valid Input", () => {
+        cy.get(".MuiInputBase-root").type('9.0.0')
+        cy.get(".MuiButtonBase-root").click()
+
+        cy.get("p").should(
+            "have.text",
+            "The release tagged with 9.0.0 was created September 14, 2022 at 4:44:59 PM CDT"
+          );
+    });
+    it("Should Display Help Text When Tag Input is Not Found", () => {
+        cy.get(".MuiInputBase-root").type('invalid')
+        cy.get(".MuiButtonBase-root").click()
+        cy.get(".MuiFormHelperText-root").should("have.text","Tag not found.")
+    });
+    
+});
+  
+  
